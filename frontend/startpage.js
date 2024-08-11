aa72e0e1f2752406b6af399b0f077f089b910b4d
Simon Hallefält <simon@hallefelt.se>
2024-08-11 17:54:50 +0200

    made number buttons and the table modifiable by user

--- frontend/startpage.js
+++ frontend/startpage.js
@@ -1,3 +1,17 @@
-function alertButton() {
-    alert("hello world!")
+function numberButton(number) {
+    alert(number)
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var cells = document.querySelectorAll('td');
+    cells.forEach(function(cell) {
+        cell.addEventListener('click', function() {
+            var currentValue = parseInt(cell.textContent);
+            if (!isNaN(currentValue)) {
+                cell.textContent = (currentValue + 1) % 10;
+            } else {
+                cell.textContent = 1
+            }
+        });
+    });
+});
